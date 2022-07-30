f0626846e97b65a069382781f73d86a427128b7d
Anypaster <Anypaster@gmail.com>
2022-07-30 09:01:13 +0200

    deb preloader

--- src/assets/common/preloader/preloader.jsx
+++ src/assets/common/preloader/preloader.jsx
@@ -3,6 +3,7 @@ import preloader from './preloader.svg'
 
 //Create a preloader component //
 const isPreloader = (isFetching) => {
+    debugger
     return (
     <>{isFetching ? <img className='users-preloader' src={preloader} alt='preloader'></img> : null}</>
     )
